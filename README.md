6a5d21f08512c55dd8a81e27b467322a931e6929
Guo Yunhe <i@guoyunhe.me>
2024-01-23 23:39:19 +0800

    first version

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 Show shadow for hidden content in scrollable area.
 
-```jsx
+```jsx device="mobile"
+import { ScrollShadow } from 'react-scroll-shadow-pro';
+import { LoremIpsum } from 'react-lorem-ipsum';
 
+function App() {
+  return (
+    <ScrollShadow style={{ maxHeight: 300 }}>
+      <div style={{ padding: 10 }}>
+        <LoremIpsum p={10} />
+      </div>
+    </ScrollShadow>
+  );
+}
 ```
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,68 @@
 import cn from 'classnames';
-import { CSSProperties, ReactNode } from 'react';
+import {
+  CSSProperties,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+} from 'react';
 import './index.css';
 
 export interface ScrollShadowProps {
+  size?: number;
+  threshold?: number;
   children?: ReactNode;
   className?: string;
   style?: CSSProperties;
 }
 
 export function ScrollShadow({
+  size = 30,
+  threshold = 100,
   children,
   className,
   style,
 }: ScrollShadowProps) {
+  const rootRef = useRef<HTMLDivElement>(null);
+  const topRef = useRef<HTMLDivElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const updateShadow = useCallback(() => {
+    if (!rootRef.current || !topRef.current || !bottomRef.current) return;
+    topRef.current.style.height =
+      Math.min((rootRef.current.scrollTop / threshold) * size, size) + 'px';
+    bottomRef.current.style.height =
+      Math.min(
+        ((rootRef.current.scrollHeight -
+          rootRef.current.clientHeight -
+          rootRef.current.scrollTop) /
+          threshold) *
+          size,
+        size,
+      ) + 'px';
+  }, []);
+
+  useEffect(() => {
+    updateShadow();
+  }, []);
+
   return (
-    <>
-      {children}
-      <div className={cn('react-scroll-shadow-pro', className)} style={style}>
-        <div className="react-scroll-shadow-pro-top"></div>
-        <div className="react-scroll-shadow-pro-bottom"></div>
+    <div
+      ref={rootRef}
+      onScroll={updateShadow}
+      className={cn('react-scroll-shadow-pro', className)}
+      style={{ ...style, '--size': size } as CSSProperties}
+    >
+      <div className="react-scroll-shadow-pro-top">
+        <div ref={topRef} className="react-scroll-shadow-pro-top-shadow"></div>
       </div>
-    </>
+      <div className="react-scroll-shadow-pro-content">{children}</div>
+      <div className="react-scroll-shadow-pro-bottom">
+        <div
+          ref={bottomRef}
+          className="react-scroll-shadow-pro-bottom-shadow"
+        ></div>
+      </div>
+    </div>
   );
 }
